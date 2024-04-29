d7e174f7f8b8e57e1beb38902316533542c37516
nxvembr <nxvembr@posteo.me>
2024-04-29 21:43:26 +0300

    feat: Pasta title and displaying user's pastas

--- instapasta/pasta.py
+++ instapasta/pasta.py
@@ -1,7 +1,7 @@
 """Blueprint handling pasta operations"""
 
 import time
-from flask import Blueprint, request, redirect, url_for, g
+from flask import Blueprint, request, redirect, url_for, g, render_template
 from instapasta.pyrebase import firebase_db
 from instapasta.auth import login_required
 
@@ -14,9 +14,26 @@ def create():
     """Uploads pasta to database"""
 
     pasta_data = {
+        "name": request.form["pastaName"] if request.form["pastaName"] else "Unnamed",
         "contents": request.form["pastaText"],
         "created_at": time.time(),
-        "user_id": g.user["localId"],
     }
-    firebase_db.child("pasta").push(pasta_data, g.user["idToken"])
+    firebase_db.child("pasta").child(g.user["localId"]).push(
+        pasta_data, g.user["idToken"]
+    )
     return redirect(url_for("index"))
+
+
+@bp.get("/my")
+@login_required
+def my_pastas():
+    """Get a list of current user's pastas"""
+
+    pastas_response = (
+        firebase_db.child("pasta")
+        .child(g.user["localId"])
+        .shallow()
+        .get(g.user["idToken"])
+    )
+
+    return render_template("pastas/my.jinja", pastas=pastas_response.val())
