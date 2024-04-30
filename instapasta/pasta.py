f165a46ac98ed7d4964a922de6fadbbe8eb99572
Alex <alxwnth@posteo.me>
2024-04-30 18:51:58 +0300

    feat: Add my pastas page

--- instapasta/pasta.py
+++ instapasta/pasta.py
@@ -1,6 +1,7 @@
 """Blueprint handling pasta operations"""
 
 import time
+from datetime import datetime
 from flask import Blueprint, request, redirect, url_for, g, render_template
 from instapasta.pyrebase import firebase_db
 from instapasta.auth import login_required
@@ -13,14 +14,16 @@ bp = Blueprint("pasta", __name__, url_prefix="/pasta")
 def create():
     """Uploads pasta to database"""
 
-    pasta_data = {
-        "name": request.form["pastaName"] if request.form["pastaName"] else "Unnamed",
-        "contents": request.form["pastaText"],
-        "created_at": time.time(),
-    }
-    firebase_db.child("pasta").child(g.user["localId"]).push(
+    pasta_name = request.form["pastaName"] if request.form["pastaName"] else "Unnamed"
+    timestamp = int(time.time())
+    pasta_id = str(timestamp) + pasta_name
+
+    pasta_data = {"contents": request.form["pastaText"]}
+
+    firebase_db.child("pasta").child(g.user["localId"]).child(pasta_id).push(
         pasta_data, g.user["idToken"]
     )
+
     return redirect(url_for("index"))
 
 
@@ -29,11 +32,19 @@ def create():
 def my_pastas():
     """Get a list of current user's pastas"""
 
-    pastas_response = (
+    pasta_response = (
         firebase_db.child("pasta")
         .child(g.user["localId"])
         .shallow()
         .get(g.user["idToken"])
     )
 
-    return render_template("pastas/my.jinja", pastas=pastas_response.val())
+    pasta_list = []
+
+    for pasta in pasta_response.val():
+        timestamp = int(pasta[0:10])
+        name = pasta[10:]
+
+        pasta_list.append((name, datetime.fromtimestamp(timestamp)))
+
+    return render_template("pastas/my.jinja", pastas=pasta_list)
