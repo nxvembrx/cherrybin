716e2df02b67fc2b310c7286ba6407945b6e316f
Alex <alxwnth@posteo.me>
2024-05-04 11:54:46 +0300

    feat: Migrate to Firestore

--- cherrybin/paste.py
+++ cherrybin/paste.py
@@ -1,47 +1,62 @@
 """Blueprint handling paste operations"""
 
-import time
-from flask import Blueprint, request, redirect, url_for, g, render_template, session
+import datetime
+from flask import Blueprint, request, redirect, url_for, g, render_template
+from google.cloud.firestore_v1.base_query import FieldFilter
 from cherrybin.pyrebase import firebase_db
 from cherrybin.auth import login_required
 from cherrybin.sqids import sqids
 from cherrybin.crypto import encrypt_paste, decrypt_paste, decrypt
-from .pyrebase import firebase_auth
+from cherrybin.firebase_admin import firestore_db
 
 bp = Blueprint("paste", __name__, url_prefix="/paste")
+pastes_ref = firestore_db.collection("pastes")
+
+
+class Paste:
+    def __init__(self, user_id, created_at, title, contents, paste_id=None):
+        self.user_id = user_id
+        self.created_at = created_at
+        self.title = title
+        self.contents = contents
+        self.paste_id = paste_id
+
+    def to_dict(self):
+        return {
+            "user_id": self.user_id,
+            "created_at": self.created_at,
+            "title": self.title,
+            "contents": self.contents,
+            "paste_id": self.paste_id,
+        }
 
 
 @bp.post("/create")
-# @login_required
 def create():
     """Uploads paste to database"""
 
-    timestamp = time.time()
-    paste_id = sqids.encode([int(timestamp)])
+    timestamp = datetime.datetime.now(tz=datetime.timezone.utc)
+    paste_id = sqids.encode([int(timestamp.timestamp())])
     user_id = 0
-    token = None
 
     if g.user is not None:
         user_id = g.user["localId"]
-        token = g.user["idToken"]
 
-    name = "Unnamed"
+    title = "Unnamed"
 
     if request.form["pasteName"]:
-        name = request.form["pasteName"]
+        title = request.form["pasteName"]
 
-    name_enc, contents_enc = encrypt_paste(name, request.form["pasteText"])
+    title_enc, contents_enc = encrypt_paste(title, request.form["pasteText"])
 
-    paste_meta = {
-        "name": name_enc,
-        "user_id": user_id,
-        "created_at": timestamp,
-    }
-
-    paste_contents = {"contents": contents_enc}
-
-    firebase_db.child("paste_meta").child(paste_id).set(paste_meta, token)
-    firebase_db.child("paste_contents").child(paste_id).set(paste_contents, token)
+    pastes_ref.document(paste_id).set(
+        Paste(
+            user_id,
+            timestamp,
+            title_enc,
+            contents_enc,
+        ).to_dict()
+    )
 
     return redirect(url_for("paste.get", paste_id=paste_id))
 
@@ -53,38 +68,43 @@ def my_pastes():
 
     user_id = g.user["localId"]
 
-    paste_response = (
-        firebase_db.child("paste_meta")
-        .order_by_child("user_id")
-        .equal_to(user_id)
-        .get(g.user["idToken"])
+    pastes = (
+        pastes_ref.where(filter=FieldFilter("user_id", "==", user_id))
+        .select(["title", "created_at", "user_id"])
+        .stream()
     )
-    paste_list = paste_response.val()
 
-    for _, paste in paste_list.items():
-        paste["name"] = decrypt(paste["name"])
+    paste_list = []
+
+    for paste in pastes:
+        paste_tmp = paste.to_dict()
+        paste_tmp["paste_id"] = paste.id
+        print(paste.id)
+        paste_tmp["title"] = decrypt(paste_tmp["title"])
+        paste_list.append(paste_tmp)
 
     return render_template("pastes/my.jinja", pastes=paste_list)
 
 
 @bp.get("/get/<string:paste_id>")
-# @login_required
 def get(paste_id):
     """Get the paste by its id"""
 
-    token = None
+    # paste = firebase_db.child("paste_meta").child(paste_id).get(token).val()
+    # paste_contents = (
+    #     firebase_db.child("paste_contents").child(paste_id).get(token)
+    # ).val()
 
-    # TODO: Refactor this into separate method (maybe)
-    if g.user is not None:
-        token = g.user["idToken"]
+    # paste.update(paste_contents)
 
-    paste = firebase_db.child("paste_meta").child(paste_id).get(token).val()
-    paste_contents = (
-        firebase_db.child("paste_contents").child(paste_id).get(token)
-    ).val()
+    paste = pastes_ref.document(paste_id).get()
 
-    paste.update(paste_contents)
-
-    paste["name"], paste["contents"] = decrypt_paste(paste["name"], paste["contents"])
-
-    return render_template("pastes/view.jinja", paste=paste)
+    if paste.exists:
+        paste = paste.to_dict()
+        paste["title"], paste["contents"] = decrypt_paste(
+            paste["title"], paste["contents"]
+        )
+        return render_template("pastes/view.jinja", paste=paste)
+    else:
+        # TODO: Throw 404
+        print("404")
